271ba09c8d4db60f77a4293919875c3844fe4caf
Utkarsh Yadav <utkarshyadav3078@gmail.com>
2025-05-18 00:05:36 +0530

    Formed Card Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FeaturedCourse from "@/components/FeaturedCourse";
 import HeroSection from "@/components/HeroSection";
 
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialised bg-grid-white/[0.0`2]">
       < HeroSection />
+      <FeaturedCourse />
     </main>
   );
 }
