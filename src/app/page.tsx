69d3fa173bef4a05f4d873892874e849dcebab07
Utkarsh Yadav <utkarshyadav3078@gmail.com>
2025-05-17 14:15:07 +0530

    Forming heroSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import HeroSection from "@/components/HeroSection";
 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialised bg-grid-white/[0.0`2]">
-      <h1 className="text-2xl text-center mt-3">Chai aur Code</h1>
+      < HeroSection />
     </main>
   );
 }
